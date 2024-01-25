57f8c9e857aed309e40ca7e650d424c84b243e7d
Eric Molina <erikhh92@gmail.com>
2024-01-25 12:22:08 +0000

    Last changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
@@ -19,6 +21,11 @@ function App() {
           Learn React!
         </a>
       </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
     </div>
   );
 }
